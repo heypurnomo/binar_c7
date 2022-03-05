b13b5704ea24af83dd2769a4c496771f0b90926f
heypurnomo <heypurnomo@gmail.com>
2022-03-05 20:04:31 +0700

    Create userRouter

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,8 +1,6 @@
 const v1 = require('express').Router();
-const user = require('./userRouter')
-const {authentication} = require('../../../middleware/authentication')
+const user = require('./userRouter');
 
-v1.use(authentication)
-v1.use('/user', user)
+v1.use('/user', user);
 
 module.exports = v1
