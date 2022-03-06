74d23d2b05768bac2aa71dcd32f473c96bd8c83e
heypurnomo <heypurnomo@gmail.com>
2022-03-06 15:44:10 +0700

    Add authentication in biodataRouter

--- routes/api/v1/biodataRouter.js
+++ routes/api/v1/biodataRouter.js
@@ -1,6 +1,8 @@
 const biodataRouter = require('express').Router();
 const BiodataController = require('../../../controller/biodataController');
+const { authentication, authorization } = require('../../../middleware');
 
+biodataRouter.use(authentication, authorization);
 biodataRouter.get('/', BiodataController.findAll);
 biodataRouter.get('/:userId', BiodataController.findByUserId);
 biodataRouter.put('/:userId', BiodataController.update);
