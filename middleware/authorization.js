7935df74cc87f585c73eea71309bf3a1a0a2c553
heypurnomo <heypurnomo@gmail.com>
2022-03-07 11:43:50 +0700

    Change authorization

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,15 +1,18 @@
 const { User } = require('../models');
 
+/**
+ * user bisa lihat punya orang lain 
+ * tapi tidak dapat update dan delete punya orang lain 
+ * admin bisa segalanya
+ */
+
 async function authorization(req, res, next) {
   try {
     const user = await User.findByPk(req.user.id);
     const id = req.url.slice(1);
-    (
-      ((req.method === 'PUT' || req.method === 'DELETE') && user.id != id)
-      || req.method === 'POST'
-    )
-    && user.role === 'user'
-    ? res.status(401).json({message: 'unauthorized'}) : next();
+    ((req.method === 'PUT' || req.method === 'DELETE') && user.id != id)
+      && user.role === 'user'
+      ? res.status(401).json({ message: 'unauthorized' }) : next();
   } catch (error) {
     res.status(500).json(error);
   }
